8ab6fb9ebb2a47ba16eac9f7e1d576382f349320
windyuuy <kugoo11v12@126.com>
2022-03-01 18:14:36 +0800

    feature: 支持外键

--- src/ExportTS.ts
+++ src/ExportTS.ts
@@ -2,12 +2,69 @@
 import { cmm, HandleSheetParams, Field, foreach, IPlugin, st, PluginBase, HandleBatchParams, OutFilePath } from "export-table-lib"
 import * as fs from "fs-extra"
 
+export class Cond {
+	protected lines: string[] = []
+	protected finished: boolean = false
+	if(cond: boolean, call: () => string) {
+		if (this.finished) {
+			return
+		}
+		if (cond) {
+			this.finished = true
+			let str = call()
+			this.lines.push(str)
+		}
+		return this
+	}
+
+	elseif(cond: boolean, call: () => string) {
+		if (this.finished) {
+			return
+		}
+		if (cond) {
+			this.finished = true
+			let str = call()
+			this.lines.push(str)
+		}
+		return this
+	}
+
+	else(call: () => string) {
+		if (this.finished) {
+			return
+		}
+		this.finished = true
+		let str = call()
+		this.lines.push(str)
+		return this
+	}
+
+	toString() {
+		return this.lines.map(l => {
+			if (l.startsWith("\n")) {
+				l = l.substring(1)
+			}
+			if (l.endsWith("\n")) {
+				l = l.substring(0, l.length - 1)
+			}
+			return l
+		}).join("")
+	}
+}
+
+export function iff(cond: boolean, call: () => string): Cond
+export function iff(cond: any, call: () => string): Cond
+export function iff(cond: boolean, call: () => string) {
+	return new Cond().if(cond, call)
+}
+
 export function export_stuff(paras: HandleSheetParams): string | null {
 	let {
 		datas,
 		fields,
 		name,
 		objects,
+		tables,
 	} = paras;
 
 	let firstLetterUpper = function (str: string) {
@@ -19,7 +76,7 @@ export function export_stuff(paras: HandleSheetParams): string | null {
 	let convMemberName = firstLetterUpper
 	let convVarName = firstLetterLower
 
-	let RowClass = firstLetterUpper(name)
+	let RowClass = firstLetterUpper(name) + "Row"
 	let initFunc = name + "Init"
 	let mapfield = fields.find(a => a.type == "key")//如果是map，则生成对应的map
 	let mapName = name + "Map"
@@ -103,27 +160,154 @@ export function export_stuff(paras: HandleSheetParams): string | null {
 		return v.describe.split("\n")
 	}
 
+	var xxteaKey = "AMhGbf0cnlMCWWviP" + name + "GOK+GK*--s8V2wUd"
+	var keyOffset = "fmrarm"
+
+	var getFieldName = function (n: string) {
+		if (n == "object" || n == "any") {
+			return n + "?"
+		}
+		return n;
+	}
+
+	var getFieldDefault = function (t: string) {
+		if (t == "any") {
+			return "undefined";
+		} else if (t == "uid") {
+			return 0;
+		} else if (t == "number") {
+			return 0
+		} else if (t == "bool") {
+			return "false"
+		} else if (t == "string") {
+			return "\"\""
+		} else if (t == "string*") {
+			return "\"\""
+		} else if (t == "object") {
+			return "null as any";
+		} else if (t == "fk") {
+			return 0;
+		} else if (t == "key") {
+			return "\"\""
+		} else if (t.endsWith("[]")) {
+			return "[]"
+		}
+		return "undefined"
+	}
+
+	var getFkFieldType = function (field: Field) {
+		return tables.find(a => a.name == field.fkTableName)!.fields!.find(a => a.name == field.fkFieldName)!.type
+	}
+
 	let mainField: Field = fields.find(f => f.type == "uid") ?? fields[0]
 	let temp = `
+${foreach(fields, f => `
+${iff((f.type == "fk" || f.type == "fk[]") && f.fkTableName != name, () => `
+import ${f.fkTableName}, { ${f.fkTableName}Row } from "./${f.fkTableName}"
+`)}
+`).trim()}
+
+type uid=number;
+type key=string;
+type fk=number;
+type bool=boolean;
+
+var fields =[
+${foreach(fields, f => `	"${f.name}",`)}
+]
 
 export class ${RowClass}{
+	
 ${foreach(fields, f => `
-	/** ${f.describe} */
-	${f.name}!: ${getFieldType(f)}
+        /**
+         * ${f.describe}
+         **/
+        ${getFieldName(f.name)}:${getFieldType(f)} = ${getFieldDefault(f.type)}
+
+${iff(f.type == "fk", () => `
+${iff(getFkFieldType(f).toLowerCase() != "uid", () => `
+        protected _fk${f.name}?:${f.fkTableName}Row[]=undefined
+        /**
+         * ${f.describe}
+         **/
+        get ${f.name}DataList(){
+            if(this._fk${f.name}===undefined){
+                if(!this.${f.name}){
+                    this._fk${f.name} = [];
+                }else{
+                    this._fk${f.name}=${f.fkTableName}.filter(a=>a.${f.fkFieldName}!=null && this.${f.name}==a.${f.fkFieldName});
+                }
+            }
+            return this._fk${f.name};
+        }
+        }
+`).else(() => `
+        protected _fk${f.name}?:${f.fkTableName}Row=undefined
+        /**
+         * ${f.describe}
+         **/
+        get ${f.name}Data(){
+            if(this._fk${f.name}===undefined){
+                this._fk${f.name}=${f.fkTableName}.find(a=>a.${f.fkFieldName}==this.${f.name});
+            }
+            return this._fk${f.name};
+        }
+`)}
+`)}
+${iff(f.type == "fk[]", () => `
+        protected _fk${f.name}?:${f.fkTableName}Row[]=undefined
+        /**
+         * ${f.describe}
+         **/
+        get ${f.name}DataList(){
+            if(this._fk${f.name}===undefined){
+                if(!this.${f.name}){
+                    this._fk${f.name} = [];
+                }else{
+                    this._fk${f.name}=${f.fkTableName}.filter(a=>a.${f.fkFieldName}!=null && this.${f.name}!.indexOf(a.${f.fkFieldName})!=-1);
+                }
+            }
+            return this._fk${f.name};
+        }
+`)}
 `)}
 }
 
-export type T${RowClass}Map = { [key: string]: ${RowClass} }
-export const ${RowClass}Map: T${RowClass}Map =
-{
-${foreach(objects, obj => `
-    "${obj[mainField.name]}" : {
-		${foreach(fields, (f) => `
-"${f.name}": ${genValue(obj[f.name], f)},
-`, " ")}
-	},
+let ${name}:${RowClass} []=[];
+
+var rowData=
+[
+${foreach(datas, d => `
+	${JSON.stringify(d)},
 `)}
+]
+
+let tableData: any[] = []
+for (let record of rowData) {
+    let obj = new ${RowClass} () as any
+    for(let i = 0;i<fields.length;i++) {
+        let key = fields[i]
+        obj[key] = record[i]
+    }
+    tableData.push(obj)
 }
+
+${iff(mapfield, () => `
+export let ${mapName}:{
+	${foreach(objects, o => `
+    /** ${JSON.stringify(o)} */
+    ${o[mapfield!.name]}:${RowClass}
+	`)}
+}={} as any`)}
+
+for(let r of tableData){
+    ${name} .push(r);
+	${iff(mapfield, () => `
+    (${mapName} as any)[r. ${mapfield}.name ] =r;
+	`)}
+}
+
+export default ${name}
 `
 	return temp
 
